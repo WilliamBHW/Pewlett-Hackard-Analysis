31629f57e1e2d850bdc11323600c4e995a12d95d
William Wang <williambohanwang@gmail.com>
2021-08-14 15:47:07 -0400

    Part2 finished with 1 csv file for mentorship.

--- Employee_Challenge.sql
+++ Employee_Challenge.sql
@@ -32,3 +32,22 @@ select count(emp_no), title
 from unique_titles
 group by title
 order by 1 desc;
+
+-- Mentorship Program
+select	distinct on(e.emp_no)
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		tl.title
+--into mentorship_eligibilty
+from employees as e
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+inner join titles as tl
+on (e.emp_no = tl.emp_no)
+where (de.to_date = '9999-01-01')
+and (e.birth_date between '1965-01-01' and '1965-12-31')
+order by e.emp_no;
